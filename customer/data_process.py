d500db8af01a25a15f2c68680a6c17a7f14a56d5
zelope <ljg980214@gmail.com>
2024-09-24 11:09:32 +0900

    merge

--- customer/data_process.py
+++ customer/data_process.py
@@ -1,3 +1,5 @@
+import pickle
+import pandas as pd
 def create_contract_end_soon_feature(data):
     if (data['contract'] == 'One year') & (data['tenure'] >= 11):
         data['contract_end_soon'] = 1
@@ -38,5 +40,21 @@ def to_binary(data):
     return data 
 
 def do_1hot(data):
+    with open('./static/models/encoder.pkl', 'rb') as f:
+        loaded_encoder = pickle.load(f)
     object_columns = ['multiplelines', 'internetservice', 'onlinesecurity', 'onlinebackup', 'deviceprotection', 'techsupport', 'streamingtv', 'streamingmovies', 'contract', 'paymentmethod', 'monthly_charges_category', 'tenure_category']
-    
+    # 유저 데이터에서 변환해야 할 열만 선택 (Series로 선택)
+    user_data_to_transform = data[object_columns]
+
+    # 변환해야 할 열에 대해 인코딩 수행 (DataFrame으로 변환 후 transform)
+    encoded_user_data = pd.DataFrame(loaded_encoder.transform([user_data_to_transform]),
+                                    columns=loaded_encoder.get_feature_names_out(object_columns))
+
+    # 나머지 열은 그대로 유지 (object_columns에 없는 열)
+    user_data_remaining = data.drop(object_columns)
+
+    # 변환된 데이터와 나머지 데이터를 합침
+    final_user_data = pd.concat([user_data_remaining, encoded_user_data], axis=1)
+
+    # 최종 결과 확인
+    return final_user_data
